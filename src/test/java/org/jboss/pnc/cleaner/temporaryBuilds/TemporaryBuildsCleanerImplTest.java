ab0582fbf27962f422e3c1c8e8b58d905bba09da
Jakub Bartecek <jbartece@redhat.com>
2019-11-14 14:39:31 +0100

    Mock Keycloak server in tests, adjust tests to work with authorization implmentation

--- src/test/java/org/jboss/pnc/cleaner/temporaryBuilds/TemporaryBuildsCleanerImplTest.java
+++ src/test/java/org/jboss/pnc/cleaner/temporaryBuilds/TemporaryBuildsCleanerImplTest.java
@@ -18,9 +18,19 @@
 package org.jboss.pnc.cleaner.temporaryBuilds;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import com.github.tomakehurst.wiremock.matching.RegexPattern;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import com.github.tomakehurst.wiremock.recording.SnapshotRecordResult;
+import com.github.tomakehurst.wiremock.standalone.MappingsLoader;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
+import com.github.tomakehurst.wiremock.stubbing.StubMappings;
 import io.quarkus.test.junit.QuarkusTest;
 import org.jboss.pnc.common.util.TimeUtils;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,7 +38,10 @@ import javax.inject.Inject;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import java.util.List;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.deleteRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
@@ -50,15 +63,33 @@ public class TemporaryBuildsCleanerImplTest {
 
     static final String BUILDS_GROUPS_ENDPOINT = "/build-config-set-records/";
 
-    static final String BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT = "/build-config-set-records/temporary-older-than-timestamp";
+    static final String BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT =
+            "/build-config-set-records/temporary-older-than-timestamp";
 
     static final String SINGLE_TEMPORARY_BUILD_GROUP_FILE = "singleTemporaryBuildGroup.json";
 
 
-    private WireMockServer wireMockServer = new WireMockServer(options().port(8082));
+    private static WireMockServer keycloakServer = new WireMockServer(options()
+            .port(8084)
+            .withRootDirectory("src/test/resources/wiremock/keycloak"));
+    
+    private WireMockServer wireMockServer = new WireMockServer(options()
+            .port(8082)
+            .withRootDirectory("src/test/resources/wiremock/general"));
+
 
     @Inject
     TemporaryBuildsCleanerImpl temporaryBuildsCleaner;
+
+    @BeforeAll
+    public static void beforeAll() {
+        keycloakServer.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        keycloakServer.stop();
+    }
 
     @BeforeEach
     public void beforeEach() {
@@ -87,6 +118,26 @@ public class TemporaryBuildsCleanerImplTest {
         // then
         wireMockServer.verify(deleteRequestedFor(urlEqualTo(BUILDS_ENDPOINT + buildId)));
         wireMockServer.verify(1, deleteRequestedFor(urlMatching(BUILDS_ENDPOINT + ".*")));
+    }
+
+    @Test
+    public void shouldAddAuthorizationHeadersWhenDeleting() {
+        // given
+        final int buildId = 100;
+        wireMockServer.stubFor(get(urlMatching(BUILDS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn(aResponse()
+                .withStatus(200)
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .withBodyFile(SINGLE_TEMPORARY_BUILD_FILE)));
+
+        wireMockServer.stubFor(delete(BUILDS_ENDPOINT + buildId).willReturn(aResponse().withStatus(200)));
+
+        // when
+        temporaryBuildsCleaner.deleteExpiredBuildRecords(TimeUtils.getDateXDaysAgo(14));
+
+        // then
+        wireMockServer.verify(1, deleteRequestedFor(urlMatching(BUILDS_ENDPOINT + ".*")).withHeader("Authorization",
+                new EqualToPattern("Bearer " +
+                        "fyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ5emxOa0tBUmZlMUVzcHZJbU9rdkVTeUttVGl6N05MTWp2Z3lSSGJEZHVBIn0.eyJqdGkiOiJjNDNhMGE5OS1lZmQ3LTQ4NDUtOTliNS0yN2VjNmJiN2IyZGQiLCJleHAiOjE1NzM5MDM4NjUsIm5iZiI6MCwiaWF0IjoxNTczNzMxMDY1LCJpc3MiOiJodHRwczovL3NlY3VyZS1zc28tbmV3Y2FzdGxlLWRldmVsLnBzaS5yZWRoYXQuY29tL2F1dGgvcmVhbG1zL3BuY3JlZGhhdCIsImF1ZCI6WyJwbmNpbmR5dWkiLCJwbmNpbmR5IiwiYWNjb3VudCJdLCJzdWIiOiIyODI0Yzk1ZS0zZjM2LTQyYjktYTljNy1hZjhkNWMyZjhjOTciLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJwbmMtb3JjaGVzdHJhdG9yIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiYjc1NDcwZWMtY2E3Ny00MTYzLThiNTktMGFjZThmMGEwM2Y1IiwiYWNyIjoiMSIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJzeXN0ZW0tdXNlciIsIm9mZmxpbmVfYWNjZXNzIiwicG93ZXItdXNlciIsInVtYV9hdXRob3JpemF0aW9uIiwidXNlciJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InBuY2luZHl1aSI6eyJyb2xlcyI6WyJwbmNpbmR5YWRtaW4iLCJwbmNpbmR5dXNlciJdfSwicG5jaW5keSI6eyJyb2xlcyI6WyJwbmNpbmR5YWRtaW4iLCJwb3dlci11c2VyIiwicG5jaW5keXVzZXIiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiIiwiY2xpZW50SWQiOiJwbmMtb3JjaGVzdHJhdG9yIiwiY2xpZW50SG9zdCI6IjE3Mi41NC4xMC4xIiwicHJlZmVycmVkX3VzZXJuYW1lIjoic2VydmljZS1hY2NvdW50LXBuYy1vcmNoZXN0cmF0b3IiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjU0LjEwLjEiLCJlbWFpbCI6InNlcnZpY2UtYWNjb3VudC1wbmMtb3JjaGVzdHJhdG9yQHBsYWNlaG9sZGVyLm9yZyJ9.b8GT6tWnlOuQpoamNnIOOvLwJgwMYrhx9p6ynDx5iOmn2FM02NXK5DGuxofFznGX3kfk1YUfD3T6Bvb234GVDwgCojk_h9_uiVjdZ-f0sBWOt9Zpa_m1p1TpC8pca4kz605oYhEZ8po9Zx6wxuvFegsK1XsnuMi2saDK0dMnWT9AdjkoFsASPdrR25LLmy53KC29hxMqrJgmQc4yYOE8f4r8eHS53u2-D5JaywyGrGiTpMH5NGbqPpKM7aPbnaPcFfmzcDTT4_U3V0vBKblZy9bn5iB-K1FOFIeegqTo4OnhDfJ5Ay_qogGUO3aBH5FmiB-RStC8vuvuzS5QsiFeKw")));
     }
 
     @Test
@@ -120,7 +171,8 @@ public class TemporaryBuildsCleanerImplTest {
     public void shouldDeleteATemporaryBuildGroup() {
         // given
         final int buildId = 102;
-        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn(aResponse()
+        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn
+                (aResponse()
                 .withStatus(200)
                 .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .withBodyFile(SINGLE_TEMPORARY_BUILD_GROUP_FILE)));
@@ -139,7 +191,8 @@ public class TemporaryBuildsCleanerImplTest {
     @Test
     public void shouldSucceedIfNoBuildGroupToBeDeleted() {
         // given
-        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn(aResponse()
+        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn
+                (aResponse()
                 .withStatus(204)
                 .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)));
 
@@ -153,7 +206,8 @@ public class TemporaryBuildsCleanerImplTest {
     @Test
     public void shouldFailSafeBuildGroupDeletionIfRemoteServerIsNotWorking() {
         // given
-        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn(aResponse()
+        wireMockServer.stubFor(get(urlMatching(BUILD_GROUPS_TEMPORARY_OLDER_THAN_ENDPOINT + ".*")).willReturn
+                (aResponse()
                 .withStatus(500)
                 .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)));
 
@@ -162,5 +216,4 @@ public class TemporaryBuildsCleanerImplTest {
 
         // then - nothing should happen
     }
-
 }
