e23c1abb2d4a5e6030ac3195fa75f5c18e4df69a
Petr Kocandrle <pkocandr@redhat.com>
2019-11-22 14:53:48 +0100

    Add javadocs for the failed builds cleaner tests

--- src/test/java/org/jboss/pnc/cleaner/builds/FailedBuildsCleanerTest.java
+++ src/test/java/org/jboss/pnc/cleaner/builds/FailedBuildsCleanerTest.java
@@ -52,7 +52,7 @@ class FailedBuildsCleanerTest {
 
     private static final String ORCH_BUILDS = "/builds";
 
-    private static final String BUILD_RECORD_FAILED_OLD_NO_CONTENTID_FILE = "buildRecordFailedOldNoContentId.json";
+    private static final String BUILD_RECORD_FAILED_NO_CONTENTID_FILE = "buildRecordFailedNoContentId.json";
 
     private static final String BUILD_RECORDS_BUILDING_FILE = "buildRecordsBuilding.json";
 
@@ -131,6 +131,10 @@ class FailedBuildsCleanerTest {
         assertEquals(0, groupNames.size());
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a failed
+     * build that ended before the limit timestamp and expects it should be cleaned.
+     */
     @Test
     public void shouldCleanOk() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36000")).willReturn(aResponse()
@@ -149,6 +153,11 @@ class FailedBuildsCleanerTest {
         assertTrue(clean);
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a failed
+     * build that ended before the limit timestamp but it is missing a build content ID, so it
+     * should be found by parsed numeric ID and expects it should be cleaned.
+     */
     @Test
     public void shouldCleanNoContentId() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36000")).willReturn(aResponse()
@@ -157,7 +166,7 @@ class FailedBuildsCleanerTest {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "/36000")).willReturn(aResponse()
                 .withStatus(200)
                 .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
-                .withBodyFile(BUILD_RECORD_FAILED_OLD_NO_CONTENTID_FILE)));
+                .withBodyFile(BUILD_RECORD_FAILED_NO_CONTENTID_FILE)));
 
         // limit is set to be after the build record end time
         Instant limit = Instant.ofEpochMilli(1573174847816L);
@@ -170,6 +179,36 @@ class FailedBuildsCleanerTest {
         assertTrue(clean);
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a failed
+     * build that ended after the limit timestamp but it is missing a build content ID, so it
+     * should be found by parsed numeric ID and expects it should NOT be cleaned.
+     */
+    @Test
+    public void shouldCleanNoContentIdTooYoung() throws CleanerException {
+        orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36000")).willReturn(aResponse()
+                .withStatus(204)));
+
+        orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "/36000")).willReturn(aResponse()
+                .withStatus(200)
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .withBodyFile(BUILD_RECORD_FAILED_NO_CONTENTID_FILE)));
+
+        // limit is set to be before the build record end time
+        Instant limit = Instant.ofEpochMilli(1573174447816L);
+        // auth token is not important for the test
+        Indy indyClient = failedBuildsCleaner.initIndy("");
+        FailedBuildsCleanerSession session = new FailedBuildsCleanerSession(indyClient, limit);
+
+        boolean clean = failedBuildsCleaner.shouldClean("build-36000", session);
+
+        assertFalse(clean);
+    }
+
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a
+     * non-existing build and expects it should be cleaned.
+     */
     @Test
     public void shouldCleanNotFound() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36002")).willReturn(aResponse()
@@ -189,6 +228,10 @@ class FailedBuildsCleanerTest {
         assertTrue(clean);
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a
+     * running build without an endTime value and expects it should NOT be cleaned.
+     */
     @Test
     public void shouldCleanBuilding() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36735")).willReturn(aResponse()
@@ -207,6 +250,10 @@ class FailedBuildsCleanerTest {
         assertFalse(clean);
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a
+     * successful build which is old enough and expects it should NOT be cleaned.
+     */
     @Test
     public void shouldCleanNotFailed() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36001")).willReturn(aResponse()
@@ -215,7 +262,7 @@ class FailedBuildsCleanerTest {
                 .withBodyFile(BUILD_RECORDS_NOT_FAILED_FILE)));
 
         // limit is set to be after the build record end time
-        Instant limit = Instant.ofEpochMilli(1573175614256L);
+        Instant limit = Instant.ofEpochMilli(1573175914256L);
         // auth token is not important for the test
         Indy indyClient = failedBuildsCleaner.initIndy("");
         FailedBuildsCleanerSession session = new FailedBuildsCleanerSession(indyClient, limit);
@@ -225,6 +272,10 @@ class FailedBuildsCleanerTest {
         assertFalse(clean);
     }
 
+    /**
+     * Tests the logic saying if a build's repos should be cleaned or not. This test checks a
+     * failed build which is too young to be cleaned and expects it should NOT be cleaned.
+     */
     @Test
     public void shouldCleanTooYoung() throws CleanerException {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36000")).willReturn(aResponse()
@@ -243,6 +294,11 @@ class FailedBuildsCleanerTest {
         assertFalse(clean);
     }
 
+    /**
+     * Tests finding of build-related generic-http repositories. It reads a list of generic-http
+     * groups which contains 3 groups matching the requested build content id. It expects to get
+     * those 3 groups in the result along with the respective remote and hosted repositories.
+     */
     @Test
     public void findGenericReposOk() {
         indyWireMockServer.stubFor(get(urlMatching(INDY_STORE_GENERIC_GROUP)).willReturn(aResponse()
@@ -269,6 +325,11 @@ class FailedBuildsCleanerTest {
         assertTrue(groupNames.contains(new StoreKey(GENERIC_PKG_KEY, StoreType.hosted, "h-nodejs-org-build-23013")));
     }
 
+    /**
+     * Tests finding of build-related generic-http repositories. It reads a list of generic-http
+     * groups which contains 1 group matching the requested build content id in old format. It
+     * expects to get the group in the result along with its remote and hosted repository.
+     */
     @Test
     public void findGenericReposOldContentId() {
         indyWireMockServer.stubFor(get(urlMatching(INDY_STORE_GENERIC_GROUP)).willReturn(aResponse()
@@ -294,6 +355,11 @@ class FailedBuildsCleanerTest {
                 "h-nodereg-com-build_org-keycloak-keycloak-connect-4-x_20181109.2045")));
     }
 
+    /**
+     * Tests finding of build-related generic-http repositories. It reads a list of generic-http
+     * groups which does not contain any groups matching the requested build content id. It
+     * expects to get an empty list back.
+     */
     @Test
     public void findGenericReposNoGroups() {
         indyWireMockServer.stubFor(get(urlMatching(INDY_STORE_GENERIC_GROUP)).willReturn(aResponse()
@@ -312,6 +378,11 @@ class FailedBuildsCleanerTest {
         assertEquals(0, groupNames.size());
     }
 
+    /**
+     * Tests the whole cleanup logic for 1 build. The build failed and is old enough. It does not
+     * have any generic repos and expects that delete for build group, hosted repo and tracking
+     * report were called.
+     */
     @Test
     public void cleanBuildIfNeededOk() {
         orchWireMockServer.stubFor(get(urlMatching(ORCH_BUILDS + "?.*q=buildContentId%3D%3Dbuild-36000")).willReturn(aResponse()
