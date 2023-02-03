3ac6e1a47dce27c1154894000c2784296e9400a1
Andrea Vibelli <avibelli@redhat.com>
2023-02-03 16:32:50 +0100

    Add the DELETE call to the archive service after a temporary build is successfully deleted

--- src/main/java/org/jboss/pnc/cleaner/startup/BeanFactory.java
+++ src/main/java/org/jboss/pnc/cleaner/startup/BeanFactory.java
@@ -26,12 +26,46 @@ import org.commonjava.o11yphant.metrics.conf.DefaultMetricsConfig;
 import org.commonjava.o11yphant.metrics.conf.MetricsConfig;
 import org.commonjava.o11yphant.metrics.sli.GoldenSignalsMetricSet;
 import org.commonjava.o11yphant.metrics.system.StoragePathProvider;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
+import org.eclipse.microprofile.config.Config;
+
+import java.net.http.HttpClient;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.net.ssl.SSLContext;
 
 @ApplicationScoped
 public class BeanFactory {
+
+    @Inject
+    Config config;
+
+    @Inject
+    ManagedExecutor executor;
+
+    private HttpClient httpClient;
+
+    @PostConstruct
+    void init() throws NoSuchAlgorithmException {
+        httpClient = java.net.http.HttpClient.newBuilder()
+                .sslContext(SSLContext.getDefault())
+                .executor(executor)
+                .connectTimeout(
+                        Duration.ofSeconds(config.getValue("archive-service.http-client.request-timeout", Long.class)))
+                .build();
+    }
+
+    @Produces
+    @ApplicationScoped
+    public HttpClient getHttpClient() {
+        return httpClient;
+    }
 
     // >>> Indy client required beans - start
 
